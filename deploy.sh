5ea2d57deacc1cda8bad46f12338f1202d6dd39d
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-03-26 14:32:13 +0800

    wav da

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+cd ~/Projects/spring-boot-azure
+./mvnw package
+cd ~/Projects/cd-nginx-java
 cp ../spring-boot-azure/target/demo-0.0.1-SNAPSHOT.jar .
 
 setup()
@@ -8,41 +10,54 @@ setup()
     
     echo "Deploying $ARTIFACT_NAME" 
 
+    # Start Backup
+    mv $ARTIFACT_NAME deployment/_deploy.jar
+    cp deployment/_deploy.jar deployment/__deploy.jar
     echo "Start the backup at $1"
-    kill $(ps -A | grep [b]ackup_jar_dpl | awk '{print $1}')
-    nohup java -jar -Dserver.port=$1 -Dname=backup_jar_dpl $ARTIFACT_NAME > logs/backup.log &
+    
+    nohup java -jar -Dserver.port=$1 -Dname=backup_jar_dpl deployment/_deploy.jar > logs/backup.log &
 
+    echo "Will wait for backup server to start"
+    sleep 50m
+    echo "Backup server started"
 
+    # Stop Main Servers
     for (( i=1; i <= $2; i++ ))
     do
-    PORT=`expr $i + $1`
+    PORT=`expr $i + $1` 
+    echo "Shutting down $PORT"
     curl -X POST localhost:$PORT/actuator/shutdown
     done
 
     kill $(ps -A | grep [d]eploy_jar_dpl | awk '{print $1}')
 
-    DATE_TIME_WITHOUT_SPACES=$(date)
-    mv deployment/deploy.jar deployment_history/$(echo ${DATE_TIME_WITHOUT_SPACES// /_}).jar
-    cp $ARTIFACT_NAME deployment/deploy.jar
+    mv deployment/__deploy.jar deployment/deploy.jar
 
+    # Start Main Servers
     for (( i=1; i <= $2; i++ ))
     do
     PORT=`expr $i + $1`
+    echo "Starting $PORT"
     nohup java -jar -Dserver.port=$PORT -Dname=deploy_jar_dpl deployment/deploy.jar > logs/server$PORT.log &
     done
 
-    
-    #echo "curl -X POST localhost:$1/actuator/shutdown"
-    rm $ARTIFACT_NAME
+
+    echo "Will wait for archivel"
+    sleep 50m
+    echo "Resume Archivel"
+
+    # Archive
+    curl -X POST localhost:$1/actuator/shutdown
+    DATE_TIME_WITHOUT_SPACES=$(date)
+    mv deployment/_deploy.jar deployment_history/$(echo ${DATE_TIME_WITHOUT_SPACES// /_}).jar
+    kill $(ps -A | grep [b]ackup_jar_dpl | awk '{print $1}')
 
 }
 
 ###
 # Main body of script starts here
 ###
-
-mkdir -p deployment_history
-mkdir -p deployment
-mkdir -p logs
+reset
 setup 1000 5
+
 
