d68b4c8d7ab32a1586098df23b1436c4316a913d
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-03-28 11:25:03 +0800

    Test now

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
-cd ~/Projects/spring-boot-azure
-./mvnw package
-cd ~/Projects/cd-nginx-java
-cp ../spring-boot-azure/target/demo-0.0.1-SNAPSHOT.jar .
 
 setup()
 {
+    mkdir -p deployment
+    mkdir -p deployment_history
+    mkdir -p logs
+
     ARTIFACT_NAME=$(ls *.jar)
     
     echo "Deploying $ARTIFACT_NAME" 
 
     # Start Backup
-    mv $ARTIFACT_NAME deployment/_deploy.jar
-    cp deployment/_deploy.jar deployment/__deploy.jar
+    mv $ARTIFACT_NAME deployment/_deploy.jar # Backup
+    cp deployment/_deploy.jar deployment/__deploy.jar # Unused
     echo "Start the backup at $1"
-    
+
+    curl -X POST localhost:$1/actuator/shutdown
     nohup java -jar -Dserver.port=$1 -Dname=backup_jar_dpl deployment/_deploy.jar > logs/backup.log &
 
     echo "Will wait for backup server to start"
-    sleep 50m
+    sleep 2m
     echo "Backup server started"
 
     # Stop Main Servers
@@ -29,11 +30,11 @@ setup()
     curl -X POST localhost:$PORT/actuator/shutdown
     done
 
-    kill $(ps -A | grep [d]eploy_jar_dpl | awk '{print $1}')
+    # kill $(ps -A | grep [d]eploy_jar_dpl | awk '{print $1}')
 
     mv deployment/__deploy.jar deployment/deploy.jar
 
-    # Start Main Servers
+    # Start Main Serversnginx
     for (( i=1; i <= $2; i++ ))
     do
     PORT=`expr $i + $1`
@@ -43,14 +44,15 @@ setup()
 
 
     echo "Will wait for archivel"
-    sleep 50m
+    sleep 2m
     echo "Resume Archivel"
 
     # Archive
+    curl localhost:$1
     curl -X POST localhost:$1/actuator/shutdown
     DATE_TIME_WITHOUT_SPACES=$(date)
     mv deployment/_deploy.jar deployment_history/$(echo ${DATE_TIME_WITHOUT_SPACES// /_}).jar
-    kill $(ps -A | grep [b]ackup_jar_dpl | awk '{print $1}')
+    # kill $(ps -A | grep [b]ackup_jar_dpl | awk '{print $1}')
 
 }
 
@@ -58,6 +60,8 @@ setup()
 # Main body of script starts here
 ###
 reset
-setup 1000 5
+setup 8000 2
+
+# 8000 8001 8002 ...
 
 
