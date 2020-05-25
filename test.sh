af2d5db9c962a35e33b2c48c7c7e083186ac08ef
apt-get2Update <thirumurugan3131@gmail.com>
2020-05-25 11:01:58 +0530

    new line upend done

--- test.sh
+++ test.sh
@@ -2,16 +2,16 @@
 
 HOST=localhost:8000
 DATE_TIME=$(date)
-
+R_TEXT='return "'$DATE_TIME'";' 
 # Change RestServiceApplication to return DATE_TIME
 # Simulator for Application Modification
-sed -i -e "s/DATETIME/$DATE_TIME/g" spring-test/src/main/java/com/example/demo/RestServiceApplication.java
-rm spring-test/src/main/java/com/example/demo/*-e
+sed -i -e "/return/c $R_TEXT" spring-test/src/main/java/com/example/demo/RestServiceApplication.java
+# rm spring-test/src/main/java/com/example/demo/*-e
 
 #building a jar
-cd spring-test
-./mvnw clean package
-cd ../
+# cd spring-test
+# ./mvnw clean package
+# cd ../
 
 # #deploy jar
 # mv spring-test/target/*.jar .
