1984c0b88f4dca0060cfb37eb21e2a31e631d1c2
apt-get2Update <thirumurugan3131@gmail.com>
2020-05-25 09:46:55 +0530

    sss

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 HOST=localhost:8000
+DATE_TIME=$(date)
 
 # Change RestServiceApplication to return DATE_TIME
 # Simulator for Application Modification
-sed -i -e "s/DATETIME/$(date)/g" spring-test/src/main/java/com/example/demo/RestServiceApplication.java
+sed -i -e "s/DATETIME/$DATE_TIME/g" spring-test/src/main/java/com/example/demo/RestServiceApplication.java
 rm spring-test/src/main/java/com/example/demo/*-e
 
 #building a jar
@@ -12,15 +13,15 @@ cd spring-test
 ./mvnw clean package
 cd ../
 
-#deploy jar
-mv spring-test/target/*.jar .
-bash deploy.sh 8000 1
+# #deploy jar
+# mv spring-test/target/*.jar .
+# bash deploy.sh 8000 1
 
-RESPONSE=$(curl $HOST) 
+# RESPONSE=$(curl $HOST) 
 
-if [ "$RESPONSE" == "$DATE_TIME" ]; then
-    echo "deployment success"
-else
-    echo "failed"
-fi
-# CURL localhost:8080 / DATE_TIME
+# if [ "$RESPONSE" == "$DATE_TIME" ]; then
+#     echo "deployment success"
+# else
+#     echo "failed"
+# fi
+# # CURL localhost:8080 / DATE_TIME
