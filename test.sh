fa750b67c7c184f5a52bfda8eb95961f658388b7
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-05-25 15:32:46 +0800

    check ippo

--- test.sh
+++ test.sh
@@ -4,10 +4,9 @@ docker-compose stop && docker-compose rm -f && docker-compose up -d
 
 HOST=localhost
 DATE_TIME=$(date)
-R_TEXT='return "'$DATE_TIME'";' 
 # Change RestServiceApplication to return DATE_TIME
 # Simulator for Application Modification
-find . -name 'RestServiceApplication.java' -print0 | xargs -0 sed -i "" "s/DATETIME/$R_TEXT/g"
+find . -name 'RestServiceApplication.java' -print0 | xargs -0 sed -i "" "s/DATETIME/$DATE_TIME/g"
 # rm spring-test/src/main/java/com/example/demo/*-e
 
 # building a jar
@@ -15,17 +14,19 @@ cd spring-test
 ./mvnw clean package
 cd ../
 
+git checkout -- spring-test/src/main/java/com/example/demo/RestServiceApplication.java
+
 #deploy jar
 mv spring-test/target/*.jar .
-bash deploy.sh 8000 1
+bash deploy.sh 8000 2
 
 RESPONSE=$(curl $HOST) 
 
 if [ "$RESPONSE" == "$DATE_TIME" ]; then
     echo "deployment success"
 else
-    echo "failed"
+    echo "failed $RESPONSE" 
 fi
 
-git checkout -- spring-test/src/main/java/com/example/demo/RestServiceApplication.java
+
 # CURL localhost:8080 / DATE_TIME
