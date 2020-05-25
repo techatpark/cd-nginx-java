d8afa408339948e67aadec9d1d2e02af6a6e4f1e
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-05-25 13:57:02 +0800

    ioop test

--- test.sh
+++ test.sh
@@ -6,22 +6,24 @@ R_TEXT='return "'$DATE_TIME'";'
 # Change RestServiceApplication to return DATE_TIME
 # Simulator for Application Modification
 sed -i -e "/return/c $R_TEXT" spring-test/src/main/java/com/example/demo/RestServiceApplication.java
-rm spring-test/src/main/java/com/example/demo/*-e
+# rm spring-test/src/main/java/com/example/demo/*-e
 
-building a jar
-cd spring-test
-./mvnw clean package
-cd ../
+# building a jar
+# cd spring-test
+# ./mvnw clean package
+# cd ../
 
-#deploy jar
-mv spring-test/target/*.jar .
-bash deploy.sh 8000 1
+# #deploy jar
+# mv spring-test/target/*.jar .
+# bash deploy.sh 8000 1
 
-RESPONSE=$(curl $HOST) 
+# RESPONSE=$(curl $HOST) 
 
-if [ "$RESPONSE" == "$DATE_TIME" ]; then
-    echo "deployment success"
-else
-    echo "failed"
-fi
+# if [ "$RESPONSE" == "$DATE_TIME" ]; then
+#     echo "deployment success"
+# else
+#     echo "failed"
+# fi
+
+git checkout -- spring-test/src/main/java/com/example/demo/RestServiceApplication.java
 # CURL localhost:8080 / DATE_TIME
