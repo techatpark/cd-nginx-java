a4feb8d408796d65091cd7e20fee3e5f85797652
apt-get2Update <thirumurugan3131@gmail.com>
2020-06-23 14:43:21 +0530

    Thiru | continues loop error fixed

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,18 @@ setup()
 
     printf "Starting the backup at $1"
     nohup java -jar -Dserver.port=$1 -Dname=backup_jar_dpl $ARTIFACT_NAME > logs/backup.log &
-
-    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:$1)" != "200" ]]; do echo -ne " ." ; sleep 5; done
+    v0=1
+    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:$1)" != "200" ]]; 
+    do 
+        if [ $v0 -eq 15 ]
+        then
+            printf "Problem in stating port $1"
+            break
+        fi
+        echo -ne " ." ; 
+        v0=`expr $v0 + 1`;
+        sleep 5; 
+    done
 
     printf "\nStarted the backup at $1"
 
@@ -20,10 +30,21 @@ setup()
     # Stop Main Servers
     for (( i=1; i <= $2; i++ ))
     do
-    PORT=`expr $i + $1` 
+    PORT=`expr $i + $1`
+    v1=1 
     printf "\nShutting down $PORT"
     curl -X POST localhost:$PORT/actuator/shutdown
-    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:$PORT)" == "200" ]]; do echo -ne " ." ; sleep 5; done
+    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:$PORT)" == "200" ]]; 
+    do
+        if [ $v1 -eq 15 ]
+        then
+            printf "Problem in shoutDown port $PORT"
+            break
+        fi 
+        echo -ne " ." ; 
+        sleep 5;
+        v1=`expr $v1 + 1`;
+    done
     done
 
     cp $ARTIFACT_NAME deployment/deploy.jar
@@ -31,10 +52,21 @@ setup()
     # Start Main Serversnginx
     for (( i=1; i <= $2; i++ ))
     do
-    PORT=`expr $i + $1`
-    printf "\nStarting $PORT"
-    nohup java -jar -Dserver.port=$PORT -Dname=deploy_jar_dpl deployment/deploy.jar > logs/server$PORT.log &
-    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:$PORT)" != "200" ]]; do echo -ne " ." ; sleep 5; done
+        PORT=`expr $i + $1`
+        printf "\nStarting $PORT"
+        v2=1
+        nohup java -jar -Dserver.port=$PORT -Dname=deploy_jar_dpl deployment/deploy.jar > logs/server$PORT.log &
+        while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:$PORT)" != "200" ]]; 
+        do 
+            if [ $v2 -eq 15 ]
+            then
+                printf "Problem in stating port $PORT"
+                break
+            fi
+            echo -ne " ." ; 
+            v2=`expr $v2 + 1`; 
+            sleep 5; 
+        done
     done
     
     # Archive
